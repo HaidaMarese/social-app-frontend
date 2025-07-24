e41ad57132659a830b650cf7131a95b6966b8af0
Haida Makouangou <hmakouan06@gmail.com>
2025-07-24 13:45:47 -0400

    Second commit

--- src/clients/backendClient.js
+++ src/clients/backendClient.js
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 
 export const backendClient = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: `$${import.meta.env.VITE_BACKEND_URL}api`,
 });
+
+
